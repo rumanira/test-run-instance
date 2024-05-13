e9b5e5d99499bfcfa5b1f19253fc124ac33a572e
Arie <arie.prastowo@gmail.com>
2024-05-13 13:33:40 +0700

    Change hardcode to tag parameter

--- run-instance-script.sh
+++ run-instance-script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-instance_name="TestScraper-$(date +%Y-%m-%d_%H:%M:%S)"
-aws ec2 run-instances --image-id your_ami_id \
---instance-type t2.micro \
---key-name your_keypair_name \
---instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
---security-group-ids your_security_group_id \
---user-data file://start-script-homepage.txt \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}}]" \
---profile your_aws_profile > /dev/null
+parameter_text="homepage"
+echo "Starting Instance Manager"
+IFS=","
+read -r -a parameters <<< "$parameter_text"
+echo "Processing ${#parameters[@]} parameter(s)"
 
-aws ec2 run-instances --image-id your_ami_id \
---instance-type t2.micro \
---key-name your_keypair_name \
---instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
---security-group-ids your_security_group_id \
---user-data file://start-script-productlist.txt \
---tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}}]" \
---profile your_aws_profile > /dev/null
+instance_name="TestScraper-$(date +%Y-%m-%d_%H:%M:%S)"
+for parameter in "${parameters[@]}"; do
+    echo "$parameter"
+    aws ec2 run-instances --image-id ami-09f7e910b38c382b8 \
+    --instance-type t2.micro \
+    --key-name rmnr-dev-key-sg \
+    --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
+    --security-group-ids sg-09f67f77703a27c65 \
+    --iam-instance-profile "Name=RmnrTestScraperEC2Role" \
+    --user-data file://start-script-x.txt \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}},{Key=TestGroup,Value=${parameter}}]" \
+    --profile arie-rmnr-mgmt > /dev/null
+done
