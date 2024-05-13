74814df4636bf99fa13795ea8c62b80d7b2e64f4
Arie <arie.prastowo@gmail.com>
2024-05-13 22:08:22 +0700

    Change test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import sys
+import time
 
 def print_argument():
     if len(sys.argv) != 2:
         print("Please provide exactly one argument.")
     else:
         print(sys.argv[1])
+        time.wait(30)
+        print("selesai")
 
 if __name__ == "__main__":
     print_argument()
