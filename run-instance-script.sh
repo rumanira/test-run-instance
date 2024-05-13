ed6b6c9b9e6fdbf185d461dd44969865ce224594
Arie <arie.prastowo@gmail.com>
2024-05-13 12:04:33 +0700

    Change hardcoded value

--- run-instance-script.sh
+++ run-instance-script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 instance_name="TestScraper-$(date +%Y-%m-%d_%H:%M:%S)"
-aws ec2 run-instances --image-id ami-02512749e345170c5 \
+aws ec2 run-instances --image-id your_ami_id \
 --instance-type t2.micro \
---key-name rmnr-dev-key-sg \
+--key-name your_keypair_name \
 --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
---security-group-ids sg-09f67f77703a27c65 \
+--security-group-ids your_security_group_id \
 --user-data file://start-script-homepage.txt \
 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}}]" \
---profile arie-rmnr-mgmt > /dev/null
+--profile your_aws_profile > /dev/null
 
-aws ec2 run-instances --image-id ami-02512749e345170c5 \
+aws ec2 run-instances --image-id your_ami_id \
 --instance-type t2.micro \
---key-name rmnr-dev-key-sg \
+--key-name your_keypair_name \
 --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
---security-group-ids sg-09f67f77703a27c65 \
+--security-group-ids your_security_group_id \
 --user-data file://start-script-productlist.txt \
 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}}]" \
---profile arie-rmnr-mgmt > /dev/null
+--profile your_aws_profile > /dev/null
