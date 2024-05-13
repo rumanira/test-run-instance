972f0bef66f573bd93003d1c8ca5997bb833ee04
Arie <arie.prastowo@gmail.com>
2024-05-13 14:01:11 +0700

    Update with loop

--- run-instance-script.sh
+++ run-instance-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-parameter_text="homepage"
+parameter_text="homepage,productlist"
 echo "Starting Instance Manager"
 IFS=","
 read -r -a parameters <<< "$parameter_text"
@@ -8,13 +8,13 @@ echo "Processing ${#parameters[@]} parameter(s)"
 instance_name="TestScraper-$(date +%Y-%m-%d_%H:%M:%S)"
 for parameter in "${parameters[@]}"; do
     echo "$parameter"
-    aws ec2 run-instances --image-id ami-09f7e910b38c382b8 \
+    aws ec2 run-instances --image-id your_ami_id \
     --instance-type t2.micro \
-    --key-name rmnr-dev-key-sg \
+    --key-name your_key_pair_name \
     --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
-    --security-group-ids sg-09f67f77703a27c65 \
-    --iam-instance-profile "Name=RmnrTestScraperEC2Role" \
+    --security-group-ids your_sg_id \
+    --iam-instance-profile "Name=YourInstance_profile_name" \
     --user-data file://start-script-x.txt \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}},{Key=TestGroup,Value=${parameter}}]" \
-    --profile arie-rmnr-mgmt > /dev/null
+    --profile your_profile > /dev/null
 done
