2d4b02a953d7deba89efab1086fd3c825cded343
Arie <arie.prastowo@gmail.com>
2024-05-13 17:42:35 +0700

    Run as ubuntu user

--- run-instance-script.sh
+++ run-instance-script.sh
@@ -8,13 +8,13 @@ echo "Processing ${#parameters[@]} parameter(s)"
 instance_name="TestScraper-$(date +%Y-%m-%d_%H:%M:%S)"
 for parameter in "${parameters[@]}"; do
     echo "$parameter"
-    aws ec2 run-instances --image-id your_ami_id \
+    aws ec2 run-instances --image-id your_ec_ami_id \
     --instance-type t2.micro \
     --key-name your_key_pair_name \
     --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
     --security-group-ids your_sg_id \
-    --iam-instance-profile "Name=YourInstance_profile_name" \
+    --iam-instance-profile "Name=your_iam_profile_name" \
     --user-data file://start-script-x.txt \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}},{Key=TestGroup,Value=${parameter}}]" \
-    --profile your_profile > /dev/null
+    --profile your_local_profile_name > /dev/null
 done
