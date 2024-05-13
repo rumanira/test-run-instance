aaea3ad4f6556603d0e7c7c7793f55bd25d27a04
Arie <arie.prastowo@gmail.com>
2024-05-13 22:50:37 +0700

    Change to placeholder

--- run-instance-script.sh
+++ run-instance-script.sh
@@ -8,13 +8,13 @@ echo "Processing ${#parameters[@]} parameter(s)"
 instance_name="TestScraper-$(date +%Y-%m-%d_%H:%M:%S)"
 for parameter in "${parameters[@]}"; do
     echo "$parameter"
-    aws ec2 run-instances --image-id - \
+    aws ec2 run-instances --image-id your_ami_id \
     --instance-type t2.micro \
-    --key-name - \
+    --key-name your_key_pair_name \
     --instance-market-options 'MarketType=spot,SpotOptions={MaxPrice=0.004}' \
-    --security-group-ids - \
-    --iam-instance-profile "Name=-" \
+    --security-group-ids your_sg_id \
+    --iam-instance-profile "Name=your_iam_role_name" \
     --user-data file://init-instance.txt \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance_name}},{Key=TestGroup,Value=${parameter}}]" \
-    --profile - > /dev/null
+    --profile your_profile_name > /dev/null
 done
