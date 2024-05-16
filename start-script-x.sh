84bdb869c72632868c3fb78e1f79327e5bb55b91
Arie <arie.prastowo@gmail.com>
2024-05-16 10:05:43 +0700

    Restart python script if restartable error occurs

--- start-script-x.sh
+++ start-script-x.sh
@@ -3,4 +3,9 @@ export TAG_NAME="TestGroup"
 export INSTANCE_ID="`ec2metadata --instance-id`"
 export TAG_VALUE="`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=$TAG_NAME" --output=text | cut -f5`"
 cd /home/ubuntu/test-scraper
-/usr/bin/python3 test.py "${TAG_VALUE}" >> app_scraper.log
+exit_code=1
+while [ $exit_code -ne 0 ]; do
+    /usr/bin/python3 test.py "${TAG_VALUE}" >> app_scraper.log
+    exit_code=$?
+    echo "Script exited with code: ${exit_code}"
+done
--- test.py
+++ test.py
@@ -1,14 +1,27 @@
 import sys
 import time
+import random
 
 def print_argument():
     if len(sys.argv) != 2:
         print("Please provide exactly one argument.")
     else:
-        print(sys.argv[1])
-        time.sleep(30)
-        print("selesai")
-        print("horee")
+        try:
+            print(sys.argv[1])
+            random_number = random.randint(1,30)
+            print(f"sleep for {random_number} seconds")
+            time.sleep(random_number)
+            print("selesai")
+            divisor = random_number % 2
+            random_number / divisor
+            print("horee")
+            raise NotImplementedError
+        except ZeroDivisionError as e:
+            print(f"a restartable error occurs: {e}")
+            sys.exit(1)
+        except:
+            print(f"non-restartable error occur")
 
 if __name__ == "__main__":
     print_argument()
+    sys.exit(0)
